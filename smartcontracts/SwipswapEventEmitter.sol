af05e0b14728215ccc4e1075256ed0adaa570992
eltneg <yorewole@gmail.com>
2020-10-21 10:32:08 +0100

    Add event emitter contract

---
+++ smartcontracts/SwipswapEventEmitter.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.6.0;
+
+// SPDX-License-Identifier: MIT
+
+contract EventEmitter {
+    event Fulfill(uint256 _requestId);
+    
+    function emitFulfill(uint256 _requestId) public returns (bool) {
+        emit Fulfill(_requestId);
+        return true;
+    }
+    
+    function emitFulfillSelector() public pure returns (bytes4 _selector) {
+        return this.emitFulfill.selector;
+    }
+}
