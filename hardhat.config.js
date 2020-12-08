f85e2c56b5077f9a53d8610fa1fa1dd5eb224e31
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2020-12-08 22:55:56 +0700

    deploy to testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,8 +12,16 @@ module.exports = {
     solidity: "0.6.9",
     defaultNetwork: "localhost",
     hardhat: {},
-    localhost: {
-        url: "http://127.0.0.1:6690",
+    networks: {
+        localhost: {
+            url: "http://127.0.0.1:6690",
+        },
+        ropsten: {
+            url: "https://ropsten.infura.io/v3/a9c2daa6167748c1ab6542469a583203"
+        },
+        goerli: {
+            url: "https://goerli.infura.io/v3/a9c2daa6167748c1ab6542469a583203"
+        }
     }
 };
 
